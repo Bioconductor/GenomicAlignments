37b50e3380573dd593d2dc5dbcd8b8e36059a385
Herve Pages <hpages@fhcrc.org>
2013-11-12 21:11:22 +0000

    Move "findSpliceOverlaps" methods for GAlignment* objects from GenomicRanges package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@83219 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findSpliceOverlaps-methods.R
+++ R/findSpliceOverlaps-methods.R
@@ -3,6 +3,29 @@
 ### -------------------------------------------------------------------------
 
 
+setMethod("findSpliceOverlaps", c("GAlignments", "GRangesList"),
+    function(query, subject, ignore.strand=FALSE, ..., cds=NULL)
+{
+    callGeneric(grglist(query, order.as.in.query=TRUE), subject,
+                ignore.strand, ..., cds=cds)
+})
+
+setMethod("findSpliceOverlaps", c("GAlignmentPairs", "GRangesList"),
+    function(query, subject, ignore.strand=FALSE, ..., cds=NULL)
+{
+### FIXME: order.as.in.query = FALSE needed for insertGaps(). If we
+### really want to use insertGaps(), we need to make it robust to
+### different orderings.
+
+### FIXME:
+### instead of relying on query.break column, maybe we should add a
+### 'splice = .gaps(query)' argument to .findSpliceOverlaps that we
+### set to introns(query) here. The downside is that a GRangesList
+### derived from GAlignmentPairs will no longer work.
+    callGeneric(grglist(query, order.as.in.query=FALSE), subject,
+                ignore.strand, ..., cds=cds)
+})
+
 setMethod("findSpliceOverlaps", c("character", "ANY"),
           function(query, subject, ignore.strand=FALSE, ...,
                    param=ScanBamParam(), singleEnd=TRUE)
---
+++ inst/unitTests/test_findSpliceOverlaps-methods.R
@@ -0,0 +1,6 @@
+## TODO: Add tests for "findSpliceOverlaps" methods defined in the
+## GenomicAlignments package i.e.:
+##   findSpliceOverlaps,GAlignments,GRangesList
+##   findSpliceOverlaps,GAlignmentPairs,GRangesList
+##   findSpliceOverlaps,character,ANY
+##   findSpliceOverlaps,BamFile,ANY
