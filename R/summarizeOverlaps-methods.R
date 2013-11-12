18b43e9c3253bb4cd4ca797321f36f2067f5c64f
Herve Pages <hpages@fhcrc.org>
2013-11-12 10:28:46 +0000

    Move summarizeOverlaps() generic and all methods from the GenomicRanges and Rsamtools packages.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@83199 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/summarizeOverlaps-methods.R
+++ R/summarizeOverlaps-methods.R
@@ -1,29 +1,182 @@
 ### =========================================================================
-### "summarizeOverlaps" methods
+### summarizeOverlaps() generic and methods
 ### -------------------------------------------------------------------------
 
 
-setMethod("summarizeOverlaps", c("GRanges", "BamFile"),
-    function(features, reads, mode, ignore.strand=FALSE, ...,
-             inter.feature=TRUE, singleEnd=TRUE, fragments=FALSE,
-             param=ScanBamParam())
+setGeneric("summarizeOverlaps", signature=c("features", "reads"),
+    function(features, reads, mode=Union, ignore.strand=FALSE, ...)
 {
-    .checkArgs(reads, singleEnd, fragments)
-    .dispatchBamFiles(features, BamFileList(reads), mode, ignore.strand, ...,
-                      inter.feature=inter.feature, singleEnd=singleEnd,
-                      fragments=fragments, param=param)
+    standardGeneric("summarizeOverlaps")
 })
 
-setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
-    function(features, reads, mode, ignore.strand=FALSE, ...,
-             inter.feature=TRUE, singleEnd=TRUE, fragments=FALSE,
-             param=ScanBamParam())
+
+### -------------------------------------------------------------------------
+### Methods for GAlignments, GAlignmentsList and GAlignmentPairs objects
+###
+
+.dispatchOverlaps <-
+    function(features, reads, mode, ignore.strand, inter.feature, ...)
 {
-    .checkArgs(reads, singleEnd, fragments)
-    .dispatchBamFiles(features, BamFileList(reads), mode, ignore.strand, ...,
-                      inter.feature=inter.feature, singleEnd=singleEnd,
-                      fragments=fragments, param=param)
+    if (ignore.strand) {
+        if (class(features) == "GRangesList") {
+            r <- unlist(features)
+            strand(r) <- "*"
+            features@unlistData <- r
+        } else {
+            strand(features) <- "*"
+        }
+    }
+    mode(features, reads, ignore.strand, inter.feature=inter.feature)
+}
+
+.summarizeOverlaps <- function(features, reads, mode, ignore.strand,
+                               ..., inter.feature=inter.feature)
+{
+    if (all(strand(reads) == "*"))
+        ignore.strand <- TRUE
+    mode <- match.fun(mode)
+    counts <- .dispatchOverlaps(features, reads, mode, ignore.strand,
+                                inter.feature=inter.feature)
+    colData <- DataFrame(object=class(reads),
+                         records=length(reads),
+                         row.names="reads")
+    SummarizedExperiment(assays=SimpleList(counts=as.matrix(counts)),
+                         rowData=features, colData=colData)
+}
+
+setMethod("summarizeOverlaps", c("GRanges", "GAlignments"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, reads, mode, ignore.strand,
+                       ..., inter.feature=inter.feature)
 })
+
+setMethod("summarizeOverlaps", c("GRangesList", "GAlignments"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, reads, mode, ignore.strand,
+                       ..., inter.feature=inter.feature)
+})
+
+setMethod("summarizeOverlaps", c("GRanges", "GAlignmentsList"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, grglist(reads, ignore.strand=TRUE),
+                       mode, ignore.strand, ...,
+                       inter.feature=inter.feature)
+})
+
+setMethod("summarizeOverlaps", c("GRangesList", "GAlignmentsList"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, grglist(reads, ignore.strand=TRUE),
+                       mode, ignore.strand, ...,
+                       inter.feature=inter.feature)
+})
+
+setMethod("summarizeOverlaps", c("GRanges", "GAlignmentPairs"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, grglist(reads), mode, ignore.strand,
+                       ..., inter.feature=inter.feature)
+})
+
+setMethod("summarizeOverlaps", c("GRangesList", "GAlignmentPairs"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE)
+{
+    .summarizeOverlaps(features, grglist(reads), mode, ignore.strand,
+                       ..., inter.feature=inter.feature)
+})
+
+
+### -------------------------------------------------------------------------
+### 'mode' functions 
+###
+
+Union <- function(features, reads, ignore.strand=FALSE, inter.feature=TRUE)
+{
+    ov <- findOverlaps(features, reads, ignore.strand=ignore.strand)
+    if (inter.feature) {
+        ## Remove ambigous reads.
+        reads_to_keep <- which(countSubjectHits(ov) == 1L)
+        ov <- ov[subjectHits(ov) %in% reads_to_keep]
+    }
+    countQueryHits(ov)
+}
+
+### Drop from 'reads' circular seqlevels that are in use in *both*: 'reads'
+### and 'features'.
+.dropCircularSeqlevelsInUse <- function(reads, features)
+{
+    seqlevels_in_use <- intersect(seqlevelsInUse(reads),
+                                  seqlevelsInUse(features))
+    seqinfo <- merge(seqinfo(reads), seqinfo(features))
+    is_circ <- isCircular(seqinfo)
+    circular_seqlevels <- names(is_circ)[is_circ]
+    seqlevels_to_drop <- intersect(seqlevels_in_use, circular_seqlevels)
+    if (length(seqlevels_to_drop) != 0L) {
+        warning("reads on circular sequence(s) '",
+                paste(seqlevels_to_drop, sep="', '"),
+                "' were ignored")
+        seqlevels(reads, force=TRUE) <- setdiff(seqlevels(reads),
+                                                seqlevels_to_drop)
+    }
+    reads
+}
+
+IntersectionStrict <- function(features, reads, ignore.strand=FALSE,
+                               inter.feature=TRUE)
+{
+    reads <- .dropCircularSeqlevelsInUse(reads, features)
+    ov <- findOverlaps(reads, features, type="within",
+                       ignore.strand=ignore.strand)
+    if (inter.feature) {
+        ## Remove ambigous reads.
+        reads_to_keep <- which(countQueryHits(ov) == 1L)
+        ov <- ov[queryHits(ov) %in% reads_to_keep]
+    }
+    countSubjectHits(ov)
+}
+
+.removeSharedRegions <- function(features, ignore.strand=FALSE)
+{
+    if (is(features, "GRanges")) {
+        regions <- disjoin(features, ignore.strand=ignore.strand)
+    } else if (is(features, "GRangesList")) {
+        regions <- disjoin(features@unlistData, ignore.strand=ignore.strand)
+    } else {
+        stop("internal error")  # should never happen
+    }
+    ov <- findOverlaps(features, regions, ignore.strand=ignore.strand)
+    regions_to_keep <- which(countSubjectHits(ov) == 1L)
+    ov <- ov[subjectHits(ov) %in% regions_to_keep]
+    ans_flesh <- regions[subjectHits(ov)]
+    ## Using 'countQueryHits(ov)' to compute the skeleton of 'ans' relies
+    ## on the assumption that the hits returned by findOverlaps() are always
+    ## ordered by query.
+    ans_eltlens <- countQueryHits(ov)
+    ans_skeleton <- PartitioningByEnd(cumsum(ans_eltlens))
+    relist(ans_flesh, ans_skeleton)
+}
+
+IntersectionNotEmpty <-  function(features, reads, ignore.strand=FALSE,
+                                  inter.feature=TRUE)
+{
+    features <- .removeSharedRegions(features, ignore.strand=ignore.strand)
+    Union(features, reads, ignore.strand=ignore.strand,
+           inter.feature=inter.feature)
+}
+
+
+### -------------------------------------------------------------------------
+### Methods for BamFiles and BamViews objects
+###
 
 .checkArgs <- function(bam, singleEnd, fragments)
 {
@@ -38,7 +191,19 @@ setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
     }
 }
 
-.dispatchOverlaps <- GenomicRanges:::.dispatchOverlaps
+.getReadFunction <- function(singleEnd, fragments)
+{
+    if (singleEnd) {
+        FUN <- readGAlignmentsFromBam
+    } else {
+        if (fragments)
+            FUN <- readGAlignmentsListFromBam
+        else
+            FUN <- readGAlignmentPairsFromBam
+    }
+    FUN
+}
+
 .countWithYieldSize <- function(FUN, features, bf, mode, ignore.strand,
                                 inter.feature, param)
 {
@@ -57,20 +222,6 @@ setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
         }
         ct
     }
-}
-
-.getReadFunction <- function(singleEnd, fragments)
-{
-    if (singleEnd) {
-        FUN <- readGAlignmentsFromBam
-    } else {
-        if (fragments)
-            FUN <- readGAlignmentsListFromBam
-        else
-            FUN <- readGAlignmentPairsFromBam
-    }
-
-    FUN
 }
 
 .dispatchBamFiles <-
@@ -108,6 +259,28 @@ setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
     SummarizedExperiment(assays=SimpleList(counts=counts),
                          rowData=features, colData=colData)
 }
+
+setMethod("summarizeOverlaps", c("GRanges", "BamFile"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE, singleEnd=TRUE, fragments=FALSE,
+             param=ScanBamParam())
+{
+    .checkArgs(reads, singleEnd, fragments)
+    .dispatchBamFiles(features, BamFileList(reads), mode, ignore.strand, ...,
+                      inter.feature=inter.feature, singleEnd=singleEnd,
+                      fragments=fragments, param=param)
+})
+
+setMethod("summarizeOverlaps", c("GRangesList", "BamFile"),
+    function(features, reads, mode, ignore.strand=FALSE, ...,
+             inter.feature=TRUE, singleEnd=TRUE, fragments=FALSE,
+             param=ScanBamParam())
+{
+    .checkArgs(reads, singleEnd, fragments)
+    .dispatchBamFiles(features, BamFileList(reads), mode, ignore.strand, ...,
+                      inter.feature=inter.feature, singleEnd=singleEnd,
+                      fragments=fragments, param=param)
+})
 
 .summarizeOverlaps_character <-
     function(features, reads, mode, ignore.strand=FALSE, ...,
