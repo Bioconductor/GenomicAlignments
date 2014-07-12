48f27597dca1a0799414fb816fb381646c48faa3
Valerie Obenchain <vobencha@fhcrc.org>
2014-07-12 22:43:35 +0000

    deprecate 'map' and 'pmap' and replace with 'mapCoords', 'pmapCoords'
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@92444 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mapCoords-methods.R
+++ R/mapCoords-methods.R
@@ -1,26 +1,33 @@
 ### =========================================================================
-### "map" methods
+### "mapCoords" methods
 ### -------------------------------------------------------------------------
 ###
 
-
 setMethod("map", c("GenomicRanges", "GAlignments"), function(from, to) {
-  to_grl <- grglist(to, drop.D.ranges=TRUE)
-  from_ol <- findOverlaps(from, to_grl, ignore.strand=TRUE, type="within")
-  to_hits <- to[subjectHits(from_ol)]
-  from_hits <- ranges(from)[queryHits(from_ol)]
-  ranges <- pmap(from_hits, to_hits)
-  space <- names(to_hits)
-  if (is.null(space))
-    space <- as.character(seq_len(length(to))[subjectHits(from_ol)])
-  new("GRangesMapping", hits = from_ol,
-      granges = GRanges(Rle(space), ranges))
+  .Deprecated(msg="map() has been deprecated. Use mapCoords() instead.")
 })
 
 setMethod("pmap", c("Ranges", "GAlignments"), function(from, to) {
-  starts <- .Call("ref_locs_to_query_locs", start(from),
+  .Deprecated(msg="pmap() has been deprecated. Use pmapCoords() instead.")
+})
+
+setMethod("mapCoords", c("GenomicRanges", "GAlignments"), function(x, to, ...) {
+  to_grl <- grglist(to, drop.D.ranges=TRUE)
+  from_ol <- findOverlaps(x, to_grl, ignore.strand=TRUE, type="within")
+  to_hits <- to[subjectHits(from_ol)]
+  from_hits <- ranges(x)[queryHits(from_ol)]
+  ranges <- pmapCoords(from_hits, to_hits)
+  space <- names(to_hits)
+  if (is.null(space))
+    space <- as.character(seq_len(length(to))[subjectHits(from_ol)])
+  GRanges(Rle(space), ranges, queryHits=queryHits(from_ol),
+          subjectHits=subjectHits(from_ol))
+})
+
+setMethod("pmapCoords", c("Ranges", "GAlignments"), function(x, to, ...) {
+  starts <- .Call("ref_locs_to_query_locs", start(x),
                   cigar(to), start(to), FALSE, PACKAGE="GenomicAlignments")
-  ends <- .Call("ref_locs_to_query_locs", end(from),
+  ends <- .Call("ref_locs_to_query_locs", end(x),
                 cigar(to), start(to), TRUE, PACKAGE="GenomicAlignments")
   ends <- pmax(ends, starts - 1L)
   IRanges(starts, ends)
