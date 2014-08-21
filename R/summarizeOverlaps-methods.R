7cfab64dd0d07ab7eb35dbc435612feb08d3db31
Valerie Obenchain <vobencha@fhcrc.org>
2014-08-21 23:03:57 +0000

    fix strand-related warning in summarizeOverlaps()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@93516 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/summarizeOverlaps-methods.R
+++ R/summarizeOverlaps-methods.R
@@ -36,8 +36,13 @@ setGeneric("summarizeOverlaps", signature=c("features", "reads"),
                                ..., inter.feature=inter.feature,
                                preprocess.reads=preprocess.reads)
 {
-    if (all(strand(reads) == "*"))
-        ignore.strand <- TRUE
+    if (class(reads) == "GRangesList") {
+        if (all(unlist(strand(reads), use.names=FALSE) == "*"))
+            ignore.strand <- TRUE
+    } else {
+        if (all(strand(reads) == "*"))
+            ignore.strand <- TRUE
+    }
     mode <- match.fun(mode)
     counts <- .dispatchOverlaps(features, reads, mode, ignore.strand,
                                 inter.feature=inter.feature,
