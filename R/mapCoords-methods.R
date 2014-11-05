db8b5054ccb1f71f20f5b622ec2e1ef2813a193f
Valerie Obenchain <vobencha@fhcrc.org>
2014-11-05 13:53:03 +0000

    re-wording on man pages; minor code reorganization
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@96506 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mapCoords-methods.R
+++ R/mapCoords-methods.R
@@ -1,39 +1,56 @@
 ### =========================================================================
-### "mapCoords" methods
+### 'mapCoords' and 'pmapCoords' methods
 ### -------------------------------------------------------------------------
 ###
 
-setMethod("mapCoords", c("GenomicRanges", "GAlignments"), function(x, to, ...) {
-  to_grl <- grglist(to, drop.D.ranges=TRUE)
-  from_ol <- findOverlaps(x, to_grl, ignore.strand=TRUE, type="within")
-  to_hits <- to[subjectHits(from_ol)]
-  from_hits <- ranges(x)[queryHits(from_ol)]
-  ranges <- pmapCoords(from_hits, to_hits)
-  space <- names(to_hits)
-  if (is.null(space))
-    space <- as.character(seq_len(length(to))[subjectHits(from_ol)])
-  GRanges(Rle(space), ranges, queryHits=queryHits(from_ol),
-          subjectHits=subjectHits(from_ol))
-})
+### Generics are in IRanges.
 
-setMethod("pmapCoords", c("Ranges", "GAlignments"), function(x, to, ...) {
-  starts <- .Call("ref_locs_to_query_locs", start(x),
-                  cigar(to), start(to), FALSE, PACKAGE="GenomicAlignments")
-  ends <- .Call("ref_locs_to_query_locs", end(x),
-                cigar(to), start(to), TRUE, PACKAGE="GenomicAlignments")
-  ends <- pmax(ends, starts - 1L)
-  IRanges(starts, ends)
-})
+### mapCoords:
 
-setGeneric("prmap", function(from, to) standardGeneric("prmap")) # not exported
+setMethod("mapCoords", c("GenomicRanges", "GAlignments"), 
+    function(x, to, ...) 
+    {
+        to_grl <- grglist(to, drop.D.ranges=TRUE)
+        from_ol <- findOverlaps(x, to_grl, ignore.strand=TRUE, type="within")
+        to_hits <- to[subjectHits(from_ol)]
+        from_hits <- ranges(x)[queryHits(from_ol)]
+        ranges <- pmapCoords(from_hits, to_hits)
+        space <- names(to_hits)
+        if (is.null(space))
+          space <- as.character(seq_len(length(to))[subjectHits(from_ol)])
+ 
+        GRanges(Rle(space), ranges, queryHits=queryHits(from_ol),
+                subjectHits=subjectHits(from_ol))
+    }
+)
 
-setMethod("prmap", c("Ranges", "GAlignments"), # not exported
-  function(from, to) {
-  starts <- .Call("query_locs_to_ref_locs", start(from),
-                  cigar(to), start(to), FALSE, PACKAGE="GenomicAlignments")
-  ends <- .Call("query_locs_to_ref_locs", end(from),
-                cigar(to), start(to), TRUE, PACKAGE="GenomicAlignments")
-  ends <- pmax(ends, starts - 1L)
-  IRanges(starts, ends)
-})
+### pmapCoords:
+
+setMethod("pmapCoords", c("Ranges", "GAlignments"), 
+    function(x, to, ...) 
+    {
+        starts <- .Call("ref_locs_to_query_locs", start(x), cigar(to), 
+                        start(to), FALSE, PACKAGE="GenomicAlignments")
+        ends <- .Call("ref_locs_to_query_locs", end(x), cigar(to), 
+                      start(to), TRUE, PACKAGE="GenomicAlignments")
+        ends <- pmax(ends, starts - 1L)
+        IRanges(starts, ends)
+    }
+)
+
+### prmap (not exported):
+
+setGeneric("prmap", function(from, to) standardGeneric("prmap"))
+
+setMethod("prmap", c("Ranges", "GAlignments"),
+    function(from, to) 
+    {
+        starts <- .Call("query_locs_to_ref_locs", start(from), cigar(to), 
+                        start(to), FALSE, PACKAGE="GenomicAlignments")
+        ends <- .Call("query_locs_to_ref_locs", end(from), cigar(to), 
+                      start(to), TRUE, PACKAGE="GenomicAlignments")
+        ends <- pmax(ends, starts - 1L)
+        IRanges(starts, ends)
+    }
+)
 
