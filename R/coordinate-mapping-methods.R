9b8ee2e2a1528997c1aca9cdee1e0f32f965650e
Valerie Obenchain <vobencha@fhcrc.org>
2015-01-09 03:10:13 +0000

    coordinate mapping:
    - add mapFromAlignments, pmapFromAlignments generics and methods
    - remove 'reverse' arg
    - implement name matching to determine pairs when mapping to genome
    - when mapping to reads (local) seqlevels are names from 'alignments'
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicAlignments@98191 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/coordinate-mapping-methods.R
+++ R/coordinate-mapping-methods.R
@@ -1,5 +1,5 @@
 ### =========================================================================
-### mapToAlignments() and pmapToAlignments()
+### coordinate mapping methods
 ### -------------------------------------------------------------------------
 ###
 
@@ -8,27 +8,75 @@
 ###
 
 setGeneric("mapToAlignments", signature=c("x", "alignments"),
-    function(x, alignments, reverse=FALSE, ...) 
+    function(x, alignments, ...) 
         standardGeneric("mapToAlignments")
 )
 
 setGeneric("pmapToAlignments", signature=c("x", "alignments"),
-    function(x, alignments, reverse=FALSE, ...) 
+    function(x, alignments, ...) 
         standardGeneric("pmapToAlignments")
 )
 
+setGeneric("mapFromAlignments", signature=c("x", "alignments"),
+    function(x, alignments, ...) 
+        standardGeneric("mapFromAlignments")
+)
+
+setGeneric("pmapFromAlignments", signature=c("x", "alignments"),
+    function(x, alignments, ...) 
+        standardGeneric("pmapFromAlignments")
+)
+
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### mapToAlignments() methods
+### mapToAlignments() and mapFromAlignments() methods
 ###
 
-.mapToAlignments <- function(x, alignments, reverse) 
+.mapFromAlignments <- function(x, alignments)
 {
     if (length(x) && length(alignments)) {
-        if (reverse)
-            FUN <- "map_query_locs_to_ref_locs"
-        else
-            FUN <- "map_ref_locs_to_query_locs"
-        map <- .Call(FUN, start(x), end(x), cigar(alignments), start(alignments))
+        ## name matching determines pairs
+        if (is.null(xNames <- names(x)) || 
+            is.null(alignmentsNames <- names(alignments)))
+            stop ("both 'x' and 'alignments' must have names")
+        match0 <- match(alignmentsNames, alignmentsNames)
+        match1 <- match(xNames, alignmentsNames)
+        group0 <- splitAsList(seq_along(alignmentsNames), match0)
+        group1 <- group0[match(na.omit(match1), names(group0))]
+        xHits <- rep(which(!is.na(match1)), elementLengths(group1))
+        alignmentsHits <- unlist(group1, use.names=FALSE)
+        if (!length(xHits <- na.omit(xHits)))
+            stop ("none of 'names(x)' are in 'names(alignments)'")
+
+        FUN <- "query_locs_to_ref_locs"
+        x <- x[xHits]
+        alignments <- alignments[alignmentsHits]
+        s <- .Call(FUN, start(x), cigar(alignments), start(alignments), FALSE)
+        e <- .Call(FUN, end(x), cigar(alignments), start(alignments), TRUE)
+        e <- pmax(e, s - 1L)
+
+        ## non-hits
+        keep <- !is.na(s) & !is.na(e)
+        seqname <- as.character(seqnames(alignments))
+        GRanges(Rle(seqname[keep]), IRanges(s[keep], e[keep], 
+                names=names(x)[keep]), 
+                xHits=xHits[keep], alignmentsHits=alignmentsHits[keep])
+    } else {
+        ans <- GRanges()
+        mcols(ans) <- DataFrame(xHits=integer(), alignmentsHits=integer())
+        ans
+    }
+}
+
+.mapToAlignments <- function(x, alignments) 
+{
+    ## FIXME: remove inner check for length
+    if (length(x) && length(alignments)) {
+        if (is.null(names(alignments)))
+            stop ("'alignments' must have names")
+        ## map all possible pairs
+        map <- .Call("map_ref_locs_to_query_locs", 
+                     start(x), end(x), cigar(alignments), 
+                     start(alignments))
         starts <- map[[1]]
         if (!all(length(starts))) {
             ans <- GRanges()
@@ -38,15 +86,15 @@ setGeneric("pmapToAlignments", signature=c("x", "alignments"),
         ends <- pmax(map[[2]], starts - 1L)
         xHits <- map[[3]]
         alignmentsHits <- map[[4]]
-        seqname <- as.character(seqnames(alignments)[alignmentsHits])
+        seqname <- names(alignments)[alignmentsHits]
+        ## non-hits
         if (any(skip <- is.na(starts) | is.na(ends))) {
             starts[skip] <- 1L 
             ends[skip] <- 0L
             seqname[skip] <- "unmapped"
         }
-        ## result is "*" strand
-        GRanges(Rle(seqname), IRanges(starts, ends), strand="*", 
-                DataFrame(xHits, alignmentsHits))
+        GRanges(Rle(seqname), IRanges(starts, ends, names=names(x)[xHits]), 
+                strand="*", DataFrame(xHits, alignmentsHits))
     } else {
         ans <- GRanges()
         mcols(ans) <- DataFrame(xHits=integer(), alignmentsHits=integer())
@@ -55,75 +103,76 @@ setGeneric("pmapToAlignments", signature=c("x", "alignments"),
 }
 
 setMethod("mapToAlignments", c("Ranges", "GAlignments"),
-    function(x, alignments, reverse=FALSE, ...)
-    {
-        if (reverse)
-            ranges(.mapToAlignments(x, alignments, TRUE))
-        else
-            ranges(.mapToAlignments(x, alignments, FALSE))
-    }
+    function(x, alignments, ...)
+        ranges(.mapToAlignments(x, alignments))
 )
 
 setMethod("mapToAlignments", c("GenomicRanges", "GAlignments"),
-    function(x, alignments, reverse=FALSE, ...)
-    {
-        if (reverse)
-            .mapToAlignments(x, alignments, TRUE)
-        else
-            .mapToAlignments(x, alignments, FALSE)
-    }
+    function(x, alignments, ...)
+        .mapToAlignments(x, alignments)
+)
+
+setMethod("mapFromAlignments", c("Ranges", "GAlignments"),
+    function(x, alignments, ...)
+        ranges(.mapFromAlignments(x, alignments))
+)
+
+setMethod("mapFromAlignments", c("GenomicRanges", "GAlignments"),
+    function(x, alignments, ...) 
+        .mapFromAlignments(x, alignments)
 )
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### pmapToAlignments() methods
+### pmapToAlignments() and pmapFromAlignments() methods
 ###
 
-.pmapToAlignments <- function(x, alignments, reverse)
+.pmapAlignments <- function(x, alignments, reverse)
 {
     if (length(x) && length(alignments)) {
         if (length(x) != length(alignments))
             stop("'x' and 'alignments' must have the same length")
 
-        if (reverse)
+        if (reverse) {
             FUN <- "query_locs_to_ref_locs"
-        else
+            seqname <- as.character(seqnames(alignments))
+        } else {
+            if (is.null(names(alignments)))
+                stop ("'alignments' must have names")
             FUN <- "ref_locs_to_query_locs"
+            seqname <- names(alignments)
+        }
         s <- .Call(FUN, start(x), cigar(alignments), start(alignments), FALSE)
         e <- .Call(FUN, end(x), cigar(alignments), start(alignments), TRUE)
         e <- pmax(e, s - 1L)
 
-        ## non-hits are zero width with seqname "unmmapped"
-        seqname <- as.character(seqnames(alignments))
+        ## non-hits
         if (any(skip <- is.na(s) | is.na(e))) {
             s[skip] <- 1L 
             e[skip] <- 0L
             seqname[skip] <- "unmapped"
         }
-        ## result is "*" strand
-        GRanges(Rle(seqname), IRanges(s, e))
+        GRanges(Rle(seqname), IRanges(s, e, names=names(x)))
     } else {
         GRanges()
     }
 }
 
 setMethod("pmapToAlignments", c("Ranges", "GAlignments"),
-    function(x, alignments, reverse=FALSE, ...)
-    {
-        if (reverse)
-            ranges(.pmapToAlignments(x, alignments, TRUE))
-        else
-            ranges(.pmapToAlignments(x, alignments, FALSE))
-    }
+    function(x, alignments, ...)
+        ranges(.pmapAlignments(x, alignments, FALSE))
 )
 
 setMethod("pmapToAlignments", c("GenomicRanges", "GAlignments"), 
-    function(x, alignments, reverse=FALSE, ...) 
-    {
-        if (reverse)
-            .pmapToAlignments(ranges(x), alignments, TRUE)
-        else
-            .pmapToAlignments(ranges(x), alignments, FALSE)
-
-    }
+    function(x, alignments, ...) 
+        .pmapAlignments(ranges(x), alignments, FALSE)
 )
 
+setMethod("pmapFromAlignments", c("Ranges", "GAlignments"),
+    function(x, alignments, ...)
+        ranges(.pmapAlignments(x, alignments, TRUE))
+)
+
+setMethod("pmapFromAlignments", c("GenomicRanges", "GAlignments"), 
+    function(x, alignments, ...) 
+        .pmapAlignments(ranges(x), alignments, TRUE)
+)
--- inst/unitTests/test_coordinate-mapping-methods.R
+++ inst/unitTests/test_coordinate-mapping-methods.R
@@ -1,67 +1,98 @@
 ## CIGAR ops M, =, X
-GR_1 <- GRanges("chr1", IRanges(c(5, 10, 20, 25), width=2), strand="+")
-cigar <- c("11M", "11=", "11X")
-GA_1 <- GAlignments(rep("chr1", 3), rep(10L, 3), cigar, strand(rep("+", 3)))
+x1 <- GRanges("chr1", IRanges(c(5, 10, 20, 25), width=2, names=LETTERS[1:4]))
+align1 <- GAlignments(rep("chr1", 3), rep(10L, 3), 
+                      c("11M", "11=", "11X"), strand(rep("+", 3)),
+                      names=letters[1:3])
 
 ## CIGAR ops S, N, D, I, H, P
-GR_2 <- GRanges("chr1", IRanges(c(1, 20), width=6), strand="+")
+x2 <- GRanges("chr1", IRanges(c(1, 20), width=6, names=LETTERS[1:2]))
 cigar <- c("1S6M1S", "3M2N3M", "3M2D3M", "3M2I3M", "1H6M1H", "1P6M1P")
-GA_2 <- GAlignments(rep("chr1", 6), rep(10L, 6), cigar, strand(rep("+", 6)))
-GA_3 <- GAlignments(rep("chr1", 6), rep(20L, 6), cigar, strand(rep("+", 6)))
+align2 <- GAlignments(rep("chr1", 6), rep(10L, 6), cigar, strand(rep("+", 6)))
+align3 <- GAlignments(rep("chr1", 6), rep(20L, 6), cigar, strand(rep("+", 6)))
+names(align2) <- names(align3) <- letters[1:6]
 
 test_mapToAlignments <- function() {
-
-    ## reverse = FALSE 
-    ans <- mapToAlignments(GR_1, GA_1, FALSE)
+    ans <- mapToAlignments(x1, align1)
     checkIdentical(start(ans), rep(1L, 3))
     checkIdentical(end(ans), rep(2L, 3))
     checkIdentical(mcols(ans)$xHits, c(rep(2L, 3)))
     checkIdentical(mcols(ans)$alignmentsHits, c(1L, 2L, 3L))
+    checkIdentical(names(ans), rep("B", 3))
+    checkIdentical(seqlevels(ans), letters[1:3])
 
-    ans <- mapToAlignments(GR_2, GA_3, FALSE)
+    ans <- mapToAlignments(x2, align3)
     checkIdentical(end(ans), c(7L, 4L, 4L, 8L, 6L, 6L)) 
     checkIdentical(mcols(ans)$alignmentsHits, as.integer(1:6))
+}
 
-    ## reverse = TRUE 
-    ans <- mapToAlignments(GR_1, GA_1, TRUE)
+test_mapFromAlignments <- function() {
+    x <- x1
+    names(x) <- rep("all", length(x)) 
+    align <- align1
+    names(align) <- rep("all", length(align)) 
+    ans <- mapFromAlignments(x, align)
     checkIdentical(start(ans), c(14L, 14L, 14L, 19L, 19L, 19L))
     checkIdentical(mcols(ans)$xHits, c(rep(1L, 3), rep(2L, 3)))
     checkIdentical(mcols(ans)$alignmentsHits, rep(1:3, 2))
+    checkIdentical(seqlevels(ans), "chr1")
+    checkIdentical(names(ans), rep("all", 6))
 
-    ans <- mapToAlignments(GR_2, GA_2, TRUE)
+    names(x) <- c("hit", "hit", "blank", "blank") 
+    names(align) <- c("BLANK", "hit", "BLANK") 
+    ans <- mapFromAlignments(x, align)
+    checkIdentical(names(ans), c("hit", "hit"))
+    checkIdentical(seqlevels(ans), "chr1")
+
+    x <- x2
+    names(x) <- rep("all", length(x)) 
+    align <- align2
+    names(align) <- rep("all", length(align)) 
+    ans <- mapFromAlignments(x, align)
     checkIdentical(start(ans), rep(10L, 6))
     checkIdentical(end(ans), c(15L, 17L, 17L, 13L, 15L, 15L)) 
     checkIdentical(mcols(ans)$alignmentsHits, as.integer(1:6))
 }
 
 test_pmapToAlignments <- function() {
-
-    ## reverse = FALSE 
-    GA_1P <- rep(GA_1[1], length(GR_1))
-    ans <- pmapToAlignments(GR_1, GA_1P, FALSE)
-    checkIdentical(length(ans), length(GR_1))
+    x <- x1
+    align <- rep(align1[1], length(x1))
+    ans <- pmapToAlignments(x, align)
+    checkIdentical(length(ans), length(x))
     checkIdentical(width(ans), c(0L, 2L, 0L, 0L))
     checkIdentical(start(ans), rep(1L, 4))
     checkIdentical(end(ans), c(0L, 2L, 0L, 0L))
+    checkIdentical(names(ans), names(x))
+    checkTrue(all(seqlevels(ans) %in% c("a", "unmapped"))) 
 
-    GR_2P <- rep(GR_2[2], length(GA_2))
-    ans <- pmapToAlignments(GR_2P, GA_3, FALSE)
+    x <- rep(x2[2], length(align3))
+    align <- align3
+    ans <- pmapToAlignments(x, align)
     checkIdentical(width(ans), c(6L, 4L, 4L, 8L, 6L, 6L))
     checkIdentical(start(ans), c(2L, rep(1L, 5))) 
     checkIdentical(end(ans), c(7L, 4L, 4L, 8L, 6L, 6L)) 
+}
 
-    ## reverse = TRUE 
-    GA_1P <- rep(GA_1[1], length(GR_1))
-    ans <- pmapToAlignments(GR_1, GA_1P, TRUE)
+test_pmapFromAlignments <- function() {
+    x <- x1
+    names(x) <- rep("all", length(x))
+    align <- rep(align1[1], length(x1))
+    names(align) <- rep("all", length(align))
+    ans <- pmapFromAlignments(x, align)
     checkTrue(ncol(mcols(ans)) == 0L)
-    checkTrue(length(ans) == length(GR_1))
+    checkTrue(length(ans) == length(x1))
     checkIdentical(width(ans), c(2L, 2L, 0L, 0L))
     checkIdentical(start(ans), c(14L, 19L, 1L, 1L))
+    checkIdentical(names(ans), names(x))
+    checkTrue(all(seqlevels(ans) %in% c("chr1", "unmapped"))) 
 
-    GR_2P <- rep(GR_2[1], length(GA_2))
-    ans <- pmapToAlignments(GR_2P, GA_2, TRUE)
+    x <- rep(x2[1], length(align2))
+    names(x) <- LETTERS[seq_along(x)]
+    align <- align2
+    ans <- pmapFromAlignments(x, align)
     checkIdentical(width(ans), c(6L, 8L, 8L, 4L, 6L, 6L))
     checkIdentical(end(ans), c(15L, 17L, 17L, 13L, 15L, 15L)) 
+    checkIdentical(names(ans), names(x))
+    checkTrue(all(seqlevels(ans) %in% "chr1")) 
 }
 
 test_ref_locs_to_query_locs <- function() {
@@ -75,12 +106,12 @@ test_ref_locs_to_query_locs <- function() {
 
     ## out of bounds
     ans_s <- .Call("ref_locs_to_query_locs", 
-                   start(GR_1[1]), cigar(GA_1[1]), 
-                   start(GA_1[1]), FALSE, 
+                   start(x1[1]), cigar(align1[1]), 
+                   start(align1[1]), FALSE, 
                    PACKAGE="GenomicAlignments")
     ans_e <- .Call("ref_locs_to_query_locs", 
-                   end(GR_1[1]), cigar(GA_1[1]), 
-                   start(GA_1[1]), TRUE, 
+                   end(x1[1]), cigar(align1[1]), 
+                   start(align1[1]), TRUE, 
                    PACKAGE="GenomicAlignments")
 
     checkIdentical(ans_s, NA_integer_)
@@ -90,12 +121,12 @@ test_ref_locs_to_query_locs <- function() {
 test_query_locs_to_ref_locs <- function() {
     ## out of bounds
     ans_s <- .Call("query_locs_to_ref_locs", 
-                   start(GR_1[4]), cigar(GA_1[1]), 
-                   start(GA_1[1]), FALSE, 
+                   start(x1[4]), cigar(align1[1]), 
+                   start(align1[1]), FALSE, 
                    PACKAGE="GenomicAlignments")
     ans_e <- .Call("query_locs_to_ref_locs", 
-                   end(GR_1[4]), cigar(GA_1[1]), 
-                   start(GA_1[1]), TRUE, 
+                   end(x1[4]), cigar(align1[1]), 
+                   start(align1[1]), TRUE, 
                    PACKAGE="GenomicAlignments")
 
     checkIdentical(ans_s, NA_integer_)
