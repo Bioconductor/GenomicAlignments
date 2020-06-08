af74c803f36d7538451977463cb363841bd0e26d
Hervé Pagès <hpages@fredhutch.org>
2020-06-08 15:50:30 -0700

    resync with S4Vectors 0.27.12

--- R/OverlapEncodings-class.R
+++ R/OverlapEncodings-class.R
@@ -16,14 +16,14 @@ setClass("OverlapEncodings",
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### vertical_slot_names()
+### parallel_slot_names()
 ###
 
-### Combine the new "vertical slots" with those of the parent class. Make
-### sure to put the new vertical slots **first**. See R/bindROWS.R file in
-### the S4Vectors package for what slots should or should not be considered
-### "vertical".
-setMethod("vertical_slot_names", "OverlapEncodings",
+### Combine the new "parallel slots" with those of the parent class. Make
+### sure to put the new parallel slots **first**. See R/Vector-class.R file
+### in the S4Vectors package for what slots should or should not be considered
+### "parallel".
+setMethod("parallel_slot_names", "OverlapEncodings",
     function(x) c("Loffset", "Roffset", "encoding", "flippedQuery",
                   callNextMethod())
 )
