ea187288c38aae5e5018025a9dddf1190a4d063b
Hervé Pagès <hpages.on.github@gmail.com>
2025-09-12 08:20:04 -0700

    GenomicAlignments 1.45.3: Drop .Call entry point map_query_locs_to_ref_locs
    .Call entry point map_query_locs_to_ref_locs was never used, didn't have
    unit tests, and was buggy (used break instead of continue). Never a good
    idea to keep dead broken code around!

--- src/GenomicAlignments.h
+++ src/GenomicAlignments.h
@@ -62,13 +62,6 @@ SEXP cigar_qnarrow(
 
 /* coordinate_mapping_methods.c */
 
-SEXP map_query_locs_to_ref_locs(
-	SEXP start,
-	SEXP end,
-	SEXP cigar,
-	SEXP pos
-);
-
 SEXP map_ref_locs_to_query_locs(
 	SEXP start,
 	SEXP end,
